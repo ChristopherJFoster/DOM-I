28bee14a4f327620697a5afee336c017c7defbd4
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-28 19:53:55 -0500

    Finally got counter to trigger test functions correctly.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,6 +1,6 @@
 let count = 0;
 
-const timer = setInterval(tick, 1000);
+const timer = setInterval(tick, 10);
 
 const ten = function() {
   console.log("TEST");
@@ -13,31 +13,25 @@ const thousand = function() {
 };
 const tenThousand = function() {
   console.log("TESTERERER");
-  //clearInterval(timer);
+  clearInterval(timer);
 };
 
 function tick() {
-  switch (count) {
-    case 10000:
-      ten();
-      hundred();
-      thousand();
-      tenThousand();
-      break;
-    case 1000:
-      ten();
-      hundred();
-      thousand();
-      break;
-    case 100:
-      ten();
-      hundred();
-      break;
-    case 10:
-      ten();
-      break;
-  }
   count += 10;
+  console.log(count);
+  if (Number.isInteger(count / 10000) === true) {
+    ten();
+    hundred();
+    thousand();
+    tenThousand();
+  } else if (Number.isInteger(count / 1000) === true) {
+    ten();
+    hundred();
+    thousand();
+  } else if (Number.isInteger(count / 100) === true) {
+    ten();
+    hundred();
+  } else if (Number.isInteger(count / 10) === true) {
+    ten();
+  }
 }
-
-tick();
